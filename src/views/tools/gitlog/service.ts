e2bc956af6e9c9e5b094b337029017b0aa176687
徐泽峰 <sirpho@foxmail.com>
2025-10-11 15:11:35 +0800

    feat: 【gitlab提交日志】接口更换

--- src/views/tools/gitlog/service.ts
+++ src/views/tools/gitlog/service.ts
@@ -6,7 +6,7 @@ import { defHttp } from '@/utils/http/axios';
 export const getLog = (data: any) => {
   return defHttp.get(
     {
-      url: `http://gitlab.wahaha.com.cn:8090/api/v4/projects/${data.value}/repository/commits`,
+      url: `https://www.h-yigroup.com/gitlab/api/v4/projects/${data.value}/repository/commits`,
       params: data,
       headers: {
         'private-token': 'nocfK6xy3cgoFB_wPm1c',
