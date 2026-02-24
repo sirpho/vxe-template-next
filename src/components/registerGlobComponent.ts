fd22bff64121cae2fd1840bcb576d43a3fb7c40e
徐泽峰 <sirpho@foxmail.com>
2026-02-24 09:58:25 +0800

    feat: FilterExtend 组件提炼

--- src/components/registerGlobComponent.ts
+++ src/components/registerGlobComponent.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue';
-import { Input, Button, Layout } from 'ant-design-vue';
+import { Input, Button, Layout, Tooltip } from 'ant-design-vue';
 import VXETable from 'vxe-table';
 import './VxeTable';
 
 export function registerGlobComponent(app: App) {
-  app.use(Input).use(Button).use(Layout).use(VXETable);
+  app.use(Input).use(Button).use(Tooltip).use(Layout).use(VXETable);
 }
